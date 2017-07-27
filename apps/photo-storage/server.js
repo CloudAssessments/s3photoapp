285257fb689ca87805c945f5c786689ab9aa5e7b
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2017-07-27 11:43:56 -0700

    [Finishes #149522055] Photos Service - Route - List Photos

--- apps/photo-storage/server.js
+++ apps/photo-storage/server.js
@@ -18,6 +18,9 @@ app.post(
   require('./middleware/upload')(DEPS)
 );
 
+// Endpoint: List Photo URLs
+app.get('/bucket/:bucket/photos', require('./middleware/listUrls')(DEPS));
+
 // Endpoint: Get Photo URL
 app.get('/bucket/:bucket/photos/:photo', require('./middleware/getUrl')(DEPS));
 
