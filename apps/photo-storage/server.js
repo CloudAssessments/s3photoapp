1464d61f9013ababf89b573200596149adfb16db
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2017-07-27 11:15:20 -0700

    [Finishes #149521981] Photos Service - Route - Upload Photo

---
+++ apps/photo-storage/server.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const busboy = require('connect-busboy');
+const s3Store = require('./stores/s3')();
+
+const PORT = process.env.API_PORT || 3001;
+const DEPS = { s3Store };
+
+app.use(busboy());
+
+app.get('/', (req, res) => {
+  res.send('welcome to the photo-storage api');
+});
+
+// Endpoint: Upload Photo (and create bucket if does not exist)
+app.post(
+  '/bucket/:bucket/photos',
+  require('./middleware/assertBucket')(DEPS),
+  require('./middleware/upload')(DEPS)
+);
+
+// catch all if a path does not exist
+app.use((req, res) => {
+  res.status(404).json({ code: 'RouteNotFound' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Photo Storage API listening on http://localhost:${PORT}`);
+});
