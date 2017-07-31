2f61d8131db6e1d1c969ab5dce77f595c16ebf93
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2017-07-31 13:58:01 -0700

    Web Client - List Photos on Homepage

---
+++ apps/web-client/app.js
@@ -0,0 +1,46 @@
+/*
+  Copyright 2017 Linux Academy
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// inject dependencies into req object
+app.use((req, res, next) => {
+  req.deps = {
+    request,
+    photoApiUrl: `http://localhost:${process.env.API_PORT}`,
+    s3Bucket: process.env.S3_BUCKET,
+  };
+  next();
+});
+
+// Routes
+app.use('/', require('./middleware/homepage'));
+
+module.exports = app;
---
+++ apps/web-client/middleware/homepage.js
@@ -0,0 +1,48 @@
+/*
+  Copyright 2017 Linux Academy
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+module.exports = (req, res) => {
+  const getPhotosUrl = `${req.deps.photoApiUrl}/bucket/${req.deps.s3Bucket}/photos`;
+
+  req.deps.request.get(getPhotosUrl, (err, response, body) => {
+    let bodyJson;
+
+    if (err) {
+      return res.render('index', { err });
+    }
+
+    if (body) {
+      try {
+        bodyJson = JSON.parse(body);
+      } catch (e) {
+        return res.render('index', {
+          err: {
+            code: 'ParseError',
+            message: `Could not parse: ${body}`,
+          },
+        });
+      }
+
+      if (response.statusCode === 200 && bodyJson && bodyJson.photos) {
+        return res.render('index', {
+          bucket: req.deps.s3Bucket,
+          urls: bodyJson.photos,
+        });
+      }
+
+      return res.render('index', { err: bodyJson });
+    }
+
+    return res.render('index', { err: 'No response body' });
+  });
+};
