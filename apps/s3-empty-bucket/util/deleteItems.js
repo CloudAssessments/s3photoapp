5487a965baace8de3e7314b5ce4db5003954721e
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2017-08-08 07:23:04 -0700

    S3 Empty Bucket Script: Add to Makefile & fix linter errors

--- apps/s3-empty-bucket/util/deleteItems.js
+++ apps/s3-empty-bucket/util/deleteItems.js
@@ -20,5 +20,5 @@ module.exports = s3 => (bucket, items, cursor) => {
     cursor ? { StartAfter: cursor } : {}
   );
 
-  return s3.deleteObjects(params).promise()
+  return s3.deleteObjects(params).promise();
 };
