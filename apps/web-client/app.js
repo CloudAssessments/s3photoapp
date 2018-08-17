a31060bd502e13e6ab6ae5c9bccf2c291ad1b772
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2018-08-16 20:33:56 -0700

    Fix unit tests

--- apps/web-client/app.js
+++ apps/web-client/app.js
@@ -26,9 +26,11 @@ const dynamodb = new DynamoDB({ region: process.env.AWS_REGION || 'us-east-1' })
 
 debugAppVars('PROCESS.ENV: ', process.env);
 
-const filterHost = process.env.FILTER_HOST || process.env.DOCKER_COMPOSE ? 'photo-filter' : 'localhost';
+const filterHost =
+  process.env.FILTER_HOST || process.env.DOCKER_COMPOSE ? 'photo-filter' : 'localhost';
 const filterPort = process.env.FILTER_PORT || '3002';
-const storageHost = process.env.STORAGE_HOST || process.env.DOCKER_COMPOSE ? 'photo-storage' : 'localhost' ;
+const storageHost =
+  process.env.STORAGE_HOST || process.env.DOCKER_COMPOSE ? 'photo-storage' : 'localhost';
 const storagePort = process.env.STORAGE_PORT || '3001';
 
 debugAppVars('EXTERNAL HOSTS/PORTS: ', {
